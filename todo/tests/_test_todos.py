280591ae9cf8cefa8bf62079cb2fef031bf94d7a
Sheena O'Connell <sheena.oconnell@gmail.com>
2024-03-24 09:31:31 +0200

    improvements

---
+++ todo/tests/_test_todos.py
@@ -0,0 +1,55 @@
+from frontend_test_mixin import FrontendTestMixin
+from todo.models import TodoItem
+
+
+class TestDisplayOnLoad(FrontendTestMixin):
+    def test_display_empty_list(self):
+        self.page.goto(self.reverse_url("index"))
+        self.page.wait_for_selector("text=Nothing to see")
+
+    def test_display_one_item(self):
+        TodoItem.objects.create(title="Test item")
+        self.page.goto(self.reverse_url("index"))
+        self.page.wait_for_selector("text=Test item")
+        self.assertTrue(self.page.get_by_test_id("todo_items_empty").is_hidden())
+
+
+class TestAddNewItem(FrontendTestMixin):
+    def test_add_new_item(self):
+        self.page.goto(self.reverse_url("index"))
+        self.page.fill("input[name=title]", "New item title")
+
+        self.page.get_by_role("button", name="Add").click()
+        self.page.wait_for_selector("text=New item title")
+
+        self.assertTrue(TodoItem.objects.filter(title="New item title").exists())
+        self.assertTrue(self.page.get_by_test_id("todo_items_empty").is_hidden())
+
+
+class TestDeleteItem(FrontendTestMixin):
+    def test_delete_item(self):
+        item = TodoItem.objects.create(title="Item to delete")
+        self.page.goto(self.reverse_url("index"))
+        self.page.wait_for_selector(f"text={item.title}")
+        self.page.get_by_test_id(f"delete_item_{item.id}").click()
+        self.page.wait_for_selector("text=Item deleted successfully")
+        self.assertFalse(TodoItem.objects.filter(title="Item to delete").exists())
+        self.assertTrue(self.page.get_by_test_id("todo_items_empty").is_visible())
+
+        self.assertFalse(TodoItem.objects.filter(title="Item to delete").exists())
+
+
+class TestToggleItem(FrontendTestMixin):
+    def test_toggle_item(self):
+        item = TodoItem.objects.create(title="Item to toggle")
+        self.page.goto(self.reverse_url("index"))
+        self.page.wait_for_selector(f"text={item.title}")
+        self.page.get_by_test_id(f"toggle_item_{item.id}").click()
+        self.page.wait_for_selector("text=Item completed")
+        item.refresh_from_db()
+        self.assertTrue(item.completed)
+
+        self.page.get_by_test_id(f"toggle_item_{item.id}").click()
+        self.page.wait_for_selector("text=Item marked as incomplete")
+        item.refresh_from_db()
+        self.assertFalse(item.completed)
--- todo/tests/test_todos.py
+++ todo/tests/test_todos.py
@@ -1,55 +1,47 @@
-from frontend_test_mixin import FrontendTestMixin
+from playwright.sync_api import Page, expect
+import os
+from django.urls import reverse
 from todo.models import TodoItem
 
-class TestDisplayOnLoad(FrontendTestMixin):
-    def test_display_empty_list(self):
-        self.page.goto(self.reverse_url("index"))
-        self.page.wait_for_selector("text=Nothing to see")
-        # self.page.screenshot(path="tests/screenshots/test_display_empty_list.png")
-        
-
-    def test_display_one_item(self):
-        TodoItem.objects.create(title="Test item")
-        self.page.goto(self.reverse_url("index"))
-        self.page.wait_for_selector("text=Test item")
-        self.assertTrue(self.page.get_by_test_id("todo_items_empty").is_hidden())
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = (
+    "true"  # <<< need to run sync code from an async context
+    #       #     models are sync
+)
 
 
-class TestAddNewItem(FrontendTestMixin):
-    def test_add_new_item(self):
-        self.page.goto(self.reverse_url("index"))
-        self.page.fill("input[name=title]", "New item title")
-        
-        self.page.get_by_role("button", name="Add").click()
-        self.page.wait_for_selector("text=New item title")
-
-        self.assertTrue(TodoItem.objects.filter(title="New item title").exists())
-        self.assertTrue(self.page.get_by_test_id("todo_items_empty").is_hidden())
-
-class TestDeleteItem(FrontendTestMixin):
-    def test_delete_item(self):
-        item = TodoItem.objects.create(title="Item to delete")
-        self.page.goto(self.reverse_url("index"))
-        self.page.wait_for_selector(f'text={item.title}')
-        self.page.get_by_test_id(f"delete_item_{item.id}").click()
-        self.page.wait_for_selector("text=Item deleted successfully")
-        self.assertFalse(TodoItem.objects.filter(title="Item to delete").exists())
-        self.assertTrue(self.page.get_by_test_id("todo_items_empty").is_visible())
-
-        self.assertFalse(TodoItem.objects.filter(title="Item to delete").exists())
+def reverse_url(
+    live_server, viewname, urlconf=None, args=None, kwargs=None, current_app=None
+):
+    end = reverse(viewname, urlconf, args, kwargs, current_app)
+    return f"{live_server.url}{end}"
 
 
-class TestToggleItem(FrontendTestMixin):
-    def test_toggle_item(self):
-        item = TodoItem.objects.create(title="Item to toggle")
-        self.page.goto(self.reverse_url("index"))
-        self.page.wait_for_selector(f'text={item.title}')
-        self.page.get_by_test_id(f"toggle_item_{item.id}").click()
-        self.page.wait_for_selector("text=Item completed")
-        item.refresh_from_db()
-        self.assertTrue(item.completed)
+def test_display_empty_list_on_first_load(live_server, page: Page):
+    page.goto(reverse_url(live_server, "index"))
+    page.wait_for_selector("text=Nothing to see")
 
-        self.page.get_by_test_id(f"toggle_item_{item.id}").click()
-        self.page.wait_for_selector("text=Item marked as incomplete")
-        item.refresh_from_db()
-        self.assertFalse(item.completed)
+
+def test_display_one_item_on_first_load(live_server, page: Page):
+    TodoItem.objects.create(title="Test item")
+    page.goto(reverse_url(live_server, "index"))
+    page.wait_for_selector("text=Test item")
+    assert page.get_by_test_id("todo_items_empty").is_hidden()
+
+
+def test_add_new_item(live_server, page: Page):
+    page.goto(reverse_url(live_server, "index"))
+    page.fill("input[name=title]", "New item title")
+
+    page.get_by_role("button", name="Add").click()
+    page.wait_for_selector("text=New item title")
+
+    assert TodoItem.objects.filter(title="New item title").exists()
+    assert page.get_by_test_id("todo_items_empty").is_hidden()
+
+
+# def test_form_clears_upon_submission(live_server, page: Page):
+# page.goto(reverse_url(live_server, "index"))
+# page.get_by_label("Title*").click()
+# page.get_by_label("Title*").fill("Foo!")
+# page.get_by_role("button", name="Add").click()
+# expect(page.get_by_label("Title*")).to_have_value("")
